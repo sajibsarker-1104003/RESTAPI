c508164a53ce1b0dd279bcfff34bdb3278bdb7d9
sajibsarker-1104003 <sajibpranto600@gmail.com>
2021-08-28 02:01:45 +0600

    Using REST API from React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import fetch from 'cross-fetch';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    //Using fetch api techniques
+    // fetch('https://jsonplaceholder.typicode.com/posts')
+    //   .then(response => response.json())
+    //   .then(data => console.log(data));
+
+    //Using axios techniques
+
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.data)
+      .then(data => console.log(data));
+
+  }
+  render() {
+    return (
+      <div>
+      <h1>Using REST API from ReactApp</h1> 
+      <p>Check it out in console </p>
+      </div>
+    );
+  }
 }
 
 export default App;
